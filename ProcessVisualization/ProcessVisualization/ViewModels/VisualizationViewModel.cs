f1a31944f531ebaa89ae9b79b1c0260ddbc14e9f
michalgornik93 <michalgornik93@gmail.com>
2021-08-01 19:24:04 +0200

    Add Visualization item to FlyoutItem

---
+++ ProcessVisualization/ProcessVisualization/ViewModels/VisualizationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ProcessVisualization.ViewModels
+{
+    public class VisualizationViewModel : BaseViewModel
+    {
+        public VisualizationViewModel()
+        {
+            Title = "Visualization";
+        }
+    }
+}
---
+++ ProcessVisualization/ProcessVisualization/Views/Visualization.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProcessVisualization.Views
+{
+    public partial class Visualization : ContentPage
+    {
+        public Visualization()
+        {
+            InitializeComponent();
+        }
+    }
+}
